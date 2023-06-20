04aad96c13095bf96195d10bbf4c8ca1e7b108fe
sezindmitrii <sezi_nh18@student.itstep.org>
2023-06-20 20:47:54 +0300

    Initial commit

---
+++ app/src/main/java/com/example/hw_activities_mult/ModelAuto.kt
@@ -0,0 +1,9 @@
+package com.example.hw_activities_mult
+
+import java.time.LocalDate
+
+data class ModelAuto
+    (var brand: String, var model : String, var year: LocalDate, var description: String, var cost: ){
+
+}
+
---
+++ app/src/main/java/com/example/hw_activities_mult/SearchFilter.kt
@@ -0,0 +1,11 @@
+package com.example.hw_activities_mult
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchFilter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_filter)
+    }
+}
